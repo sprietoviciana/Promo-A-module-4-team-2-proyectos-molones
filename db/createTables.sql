f87051bcce1b2a2159249a1fbd19ff8cd3845695
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-12-04 12:52:33 +0100

    Data Base

---
+++ db/createTables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE proyects (
+`idProyect` int auto_increment primary key,
+`name` varchar(45) not null,
+`slogan` varchar(100) not null,
+`technologies` varchar(45) not null,
+`repo` LONGTEXT not null,
+`demo` LONGTEXT,
+`desc` LONGTEXT,
+`image` LONGTEXT not null
+);
+
+CREATE TABLE autors(
+`idAutor` int auto_increment primary key,
+`autor`VARCHAR(45) not null,
+`job`VARCHAR(45) not null,
+`photo`LONGTEXT not null
+);
