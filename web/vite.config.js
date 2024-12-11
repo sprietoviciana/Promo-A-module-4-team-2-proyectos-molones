22a399e1e06d282a84b2d332f1cd2f29803db1d3
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-12-11 14:22:58 +0100

    Pages

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: "PROMO-A-MODULE-4-TEAM-2-PROYECTOS-MOLONES-BACK-"
 });
